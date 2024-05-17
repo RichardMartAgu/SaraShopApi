3a4f9d50f81d5a8631c614b63424948ffa354875
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2024-05-17 11:07:37 +0200

    Initial commit

---
+++ src/main/java/es/svalero/SaraShopApi/SaraShopApiApplication.java
@@ -0,0 +1,13 @@
+package es.svalero.SaraShopApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaraShopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/svalero/SaraShopApi/SaraShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.svalero.SaraShopApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaraShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
